e78fa45eef017841385c46c7882777a38f03333f
shubh30 <shubhamnagpal30@gmail.com>
2024-04-21 12:38:11 +0530

    Add footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href='/'>
+          <Image src='/assets/images/logo.svg' alt='logo' width={128} height={38} className='cursor-pointer' />
+        </Link>
+        <p>{currentYear} Evently. All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
